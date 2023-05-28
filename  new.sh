201268da357bfe86a3af3eebd2e8e0d9f072f9ad
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2023-05-28 20:18:38 +0530

    initial commit

---
+++  new.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+template="$1"
+new="$2"
+
+if test ! -d $template -o -z "$new" -o -e "$new"; then
+	echo "Usage: new.sh p2016_01 p2016_02"
+	exit 1
+fi
+
+set -x
+cp -fr ./$template ./$new
+gfind ./$new -type f -printf "gsed -i \"s/$template/$new/g\" %p\n" | bash
+gsed -i "/members/ a \    \"$new\"," ./Cargo.toml
+git add .
