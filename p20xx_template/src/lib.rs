53841ffdf635389390d7947eb14003e3204cff5d
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-12 20:18:37 +0530

    Update template

--- p20xx_template/src/lib.rs
+++ p20xx_template/src/lib.rs
@@ -5,6 +5,13 @@ pub fn part_a(input: &str) -> i64 {
     0
 }
 
+pub fn part_b(input: &str) -> i64 {
+    for line in input.trim().split('\n') {
+        //
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     // #[test]
@@ -15,5 +22,11 @@ mod tests {
     #[test]
     fn part_a() {
         assert_eq!(super::part_a(include_str!("input.txt")), 0);
+    }
+
+    
+    #[test]
+    fn part_b() {
+        assert_eq!(super::part_b(include_str!("input.txt")), 0);
     }
 }
