9306ee20aeca6326087445e9ce678fe0db85956f
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-12 17:31:49 +0530

    Add template

---
+++ p20xx_template/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn part_a(input: &str) -> i64 {
+    for line in input.trim().split('\n') {
+        //
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    // #[test]
+    // fn example1() {
+    //     assert_eq!(super::part_a("\n"), 0);
+    // }
+
+    #[test]
+    fn part_a() {
+        assert_eq!(super::part_a(include_str!("input.txt")), 0);
+    }
+}
