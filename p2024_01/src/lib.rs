55745348e23a34338be543abf0047c7002862164
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-12 20:16:50 +0530

    Cleanup

--- p2024_01/src/lib.rs
+++ p2024_01/src/lib.rs
@@ -4,18 +4,15 @@ pub fn part_a(input: &str) -> i64 {
     let mut v2: Vec<i64> = Vec::new();
     let mut result = 0;
     for line in input.trim().split('\n') {
-        //
         let strings: Vec<_> = line.split(' ').collect();
         let v : Vec<i64> = strings.iter().flat_map(|x| x.parse()).collect();
         v1.push(v[0]);
         v2.push(v[1]);
-        // result += (vec[i][0] - vec[i][1]).abs();
     }
     v1.sort();
     v2.sort();
     for it in v1.iter().zip(v2.iter()) {
         let (a,b) = it;
-        println!("{} {}",a,b);
         result += (a-b).abs();
     }
 
@@ -27,12 +24,10 @@ pub fn part_b(input: &str) -> i64 {
     let mut v2: Vec<i64> = Vec::new();
     let mut result = 0;
     for line in input.trim().split('\n') {
-        //
         let strings: Vec<_> = line.split(' ').collect();
         let v : Vec<i64> = strings.iter().flat_map(|x| x.parse()).collect();
         v1.push(v[0]);
         v2.push(v[1]);
-        // result += (vec[i][0] - vec[i][1]).abs();
     }
     v1.sort();
     v2.sort();
