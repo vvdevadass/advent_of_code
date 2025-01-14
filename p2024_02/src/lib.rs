7d2dc75bd92e6b49675ecddaa984daedf8093dcc
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-14 06:58:55 +0530

    Solve 2024 day 2 part 2

--- p2024_02/src/lib.rs
+++ p2024_02/src/lib.rs
@@ -1,53 +1,61 @@
+fn safety_check(report: &Vec<i64> ) -> bool {
+    let mut safe = false;
+    let mut dir: Option<bool> = None;
+    let mut count = 0;
+    for (i, &a) in report.iter().enumerate() {
+        if i+1>=report.len() {
+            break;
+        }
+
+        if (a-report[i+1]).abs() < 4 {
+            if dir == None {
+                count+=1;
+                if a > report[i+1] {
+                    dir = Some(false);
+                }
+                else if a < report[i+1] {
+                    dir = Some(true);
+                }
+                else {
+                    break;
+                }
+            }
+            else if dir == Some(false) {
+                if a > report[i+1] {
+                    count+=1;
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+            else {
+                if a < report[i+1] {
+                    count+=1;
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        else {
+            break;
+        }
+    }
+    
+    if count == report.len()-1 {
+        safe=true;
+    }
+    safe
+}
+
 pub fn part_a(input: &str) -> i64 {
     let mut res = 0;
     for line in input.trim().split('\n') {
         let strings: Vec<&str> = line.split(' ').collect();
         let v: Vec<i64> = strings.iter().flat_map(|x| x.parse()).collect();
-        let mut dir: Option<bool> = None;
-        let mut count = 0;
-        for (i, &a) in v.iter().enumerate() {
-            if i+1>=v.len() {
-                break;
-            }
-
-            if (a-v[i+1]).abs() < 4 {
-                if dir == None {
-                    count+=1;
-                    if a > v[i+1] {
-                        dir = Some(false);
-                    }
-                    else if a < v[i+1] {
-                        dir = Some(true);
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else if dir == Some(false) {
-                    if a > v[i+1] {
-                        count+=1;
-                        continue;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    if a < v[i+1] {
-                        count+=1;
-                        continue;
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-            else {
-                break;
-            }
-        }
-
-        if count == v.len()-1 {
+        if safety_check(&v) == true {
             res+=1;
         }
     }
@@ -55,27 +63,38 @@ pub fn part_a(input: &str) -> i64 {
 }
 
 pub fn part_b(input: &str) -> i64 {
+    let mut res = 0;
     for line in input.trim().split('\n') {
-        //
+        let strings: Vec<&str> = line.split(' ').collect();
+        let v: Vec<i64> = strings.iter().flat_map(|x| x.parse()).collect();
+        if safety_check(&v) == true {
+            res+=1;
+        }
+        else {
+            for (i,_) in v.iter().enumerate() {
+                let mut w = v.clone();
+                w.remove(i);
+                if safety_check(&w) == true {
+                    res+=1;
+                    break;
+                }
+            }
+        }
     }
-    0
+    res
 }
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn example1() {
-    //     assert_eq!(super::part_a("\n"), 0);
-    // }
-
     #[test]
     fn part_a() {
+        assert_eq!(super::part_b(include_str!("sample.txt")), 2);
         assert_eq!(super::part_a(include_str!("input.txt")), 534);
     }
 
-    
     #[test]
     fn part_b() {
-        assert_eq!(super::part_b(include_str!("input.txt")), 0);
+        assert_eq!(super::part_b(include_str!("sample.txt")), 4);
+        assert_eq!(super::part_b(include_str!("input.txt")), 577);
     }
 }
