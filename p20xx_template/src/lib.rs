f62eda4a344441e708f7a4fe418806f85c144c0b
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-14 07:05:50 +0530

    Update template

--- p20xx_template/src/lib.rs
+++ p20xx_template/src/lib.rs
@@ -14,19 +14,16 @@ pub fn part_b(input: &str) -> i64 {
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn example1() {
-    //     assert_eq!(super::part_a("\n"), 0);
-    // }
-
     #[test]
     fn part_a() {
+        assert_eq!(super::part_a(include_str!("sample.txt")), 0);
         assert_eq!(super::part_a(include_str!("input.txt")), 0);
     }
 
     
     #[test]
     fn part_b() {
+        assert_eq!(super::part_a(include_str!("sample.txt")), 0);
         assert_eq!(super::part_b(include_str!("input.txt")), 0);
     }
 }
