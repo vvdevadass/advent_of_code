8172e8f05c589e5610f9f719211620d7b9cc58c5
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-16 03:01:46 +0530

    Add 2024 day 5

---
+++ p2024_05/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn part1(input: &str) -> i64 {
+    for line in input.trim().split('\n') {
+        //
+    }
+    0
+}
+
+pub fn part2(input: &str) -> i64 {
+    for line in input.trim().split('\n') {
+        //
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(include_str!("sample.txt")), 0);
+        assert_eq!(super::part1(include_str!("input.txt")), 0);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(include_str!("sample.txt")), 0);
+        assert_eq!(super::part2(include_str!("input.txt")), 0);
+    }
+}
