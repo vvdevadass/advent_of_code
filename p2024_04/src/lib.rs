197194c7d528003a347465a1d9358433dc1a3f6d
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-15 23:18:13 +0530

    2024 day 4 part 2 unsolved

--- p2024_04/src/lib.rs
+++ p2024_04/src/lib.rs
@@ -136,10 +136,102 @@ pub fn part_a(input: &str) -> i64 {
 }
 
 pub fn part_b(input: &str) -> i64 {
+    let mut arr: Vec<char> = Vec::new();
+    let mut width: usize = 0;
+    let mut height: usize = 0;
+    let mut res = 0;
+
+    let dirs = [
+        "vert M left",
+        "vert M right",
+        "horz M left" ,
+        "horz M right"
+    ];
+
     for line in input.trim().split('\n') {
-        //
+        if width == 0 {
+            width = line.len();
+        }
+        for c in line.chars() {
+            arr.push(c);
+        }
+        height+=1;
     }
-    0
+
+    for (i,&a) in arr.iter().enumerate() {
+        if a == 'M' {
+            let row: i32 = i as i32 /width as i32;
+            let col: i32 = i as i32 %width as i32;
+            let h = height as i32;
+            let w = width as i32;
+            let mut j: i32; // second M
+            let mut k: i32; // A
+            let mut l: i32; // second S
+            let mut n: i32; // first S
+            // println!("{h},{w}");
+
+            for dir in dirs {
+                // update j,k,l based on direction
+                match dir {
+                    "diag left up"  => {
+                        if row - 2 >= 0 && col - 2 >= 0 {
+                            j = (row)*w + col-2;
+                            k = (row - 1)*w + col-1;
+                            l = (row - 2)*w + col-2;
+                            n = (row - 2)*w + col;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    "diag left down"  => {
+                        if row + 2 < h && col - 2 >= 0 {
+                            j = (row + 2)*w + col;
+                            k = (row + 1)*w + col-1;
+                            l = (row + 2)*w + col-2;
+                            n = (row)*w + col-2;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    "diag right up"  => {
+                        if row - 2 >= 0 && col + 2 < w {
+                            j = (row)*w + col+2;
+                            k = (row - 1)*w + col+1;
+                            l = (row - 2)*w + col+2;
+                            n = (row - 2)*w + col;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    "diag right down" => {
+                        if row + 2 < h && col + 2 < w {
+                            // down
+                            j = (row+2)*w + col;
+                            k = (row+1)*w + col+1;
+                            l = (row+2)*w + col+2;
+                            n = (row)*w + col+2;
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    _ => {
+                        continue;
+                    }
+                }
+
+                let s = format!("{}{}{}", a, arr[k as usize], arr[l as usize]);
+                let s2 = format!("{}{}{}", arr[j as usize], arr[k as usize], arr[n as usize]);
+                if s.as_str() == "MAS" && s2.as_str() == "MAS" {
+                    res+=1;
+                }
+            }
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -153,7 +245,7 @@ mod tests {
 
     #[test]
     fn part_b() {
-        assert_eq!(super::part_a(include_str!("sample.txt")), 0);
+        assert_eq!(super::part_b(include_str!("sample.txt")), 9);
         assert_eq!(super::part_b(include_str!("input.txt")), 0);
     }
 }
