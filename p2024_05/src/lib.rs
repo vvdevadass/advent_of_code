ca6e1a1293f77e2b4633e8e77ec02dec635b8bc8
Vishnu Vardhan Devadass <50523278+vvdevadass@users.noreply.github.com>
2025-01-17 22:29:41 +0530

    2024 day 5 part 2 unsolved

--- p2024_05/src/lib.rs
+++ p2024_05/src/lib.rs
@@ -27,10 +27,38 @@ pub fn part1(input: &str) -> i64 {
 }
 
 pub fn part2(input: &str) -> i64 {
+    let mut page_rules: Vec<Vec<&str>> = Vec::new();
+    let mut res = 0;
     for line in input.trim().split('\n') {
-        //
+        if line.contains("|") {
+            let x = line.split("|").collect();
+            page_rules.push(x);
+        }
+        else if line.contains(","){
+            let mut pages: Vec<&str> = line.split(",").collect();
+            let mut is_incorrect = false;
+            for i in 0..pages.len()-1 {
+                let s = vec![pages[i],pages[i+1]];
+                let s2 = vec![pages[i+1],pages[i+1]];
+                if !page_rules.contains(&s) && page_rules.contains(&s2) {
+                    // break;
+                    let a = pages[i+1];
+                    pages[i+1] = pages[i];
+                    pages[i] = a;
+                    // res+=a.parse::<i64>().unwrap();
+                    is_incorrect = true;
+                }
+
+                if ( i == (pages.len()-2) ) && is_incorrect {
+                    let &x = pages.get(pages.len()/2).unwrap_or(&"0");
+                    let x = x.parse::<i64>().unwrap();
+                    println!("{}", x);
+                    res+= x;
+                }
+            }
+        }
     }
-    0
+    res
 }
 
 #[cfg(test)]
@@ -44,7 +72,7 @@ mod tests {
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(include_str!("sample.txt")), 0);
+        assert_eq!(super::part2(include_str!("sample.txt")), 123);
         assert_eq!(super::part2(include_str!("input.txt")), 0);
     }
 }
